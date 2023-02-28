896a27f7103b84d0fb190dedc5d8924e23b78f9d
Saikat Acharjee Joy <joy29940@gmail.com>
2023-02-28 12:57:49 +0600

    cf_recent_action

---
+++ Codeforces/A_Recent_Actions.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+
+int main() {
+    FIO;
+
+    testCase(t) {
+        int n, m;
+        cin >> n >> m;
+
+        int temp = n;
+        int removed[n + 5];
+        for (int i = 0; i <= n; i++) {
+            removed[i] = -1;
+        }
+
+        int arr[m];
+        for (int i = 0; i < m; i++) {
+            cin >> arr[i];
+        }
+
+        set<int> st;
+        for (int i = 0; i < m; i++) {
+            if (st.count(arr[i]) == 1) {
+                continue;
+            } else {
+                removed[n--] = i + 1;
+            }
+
+            st.insert(arr[i]);
+        }
+
+        for (int i = 1; i <= temp; i++) {
+            cout << removed[i] << " ";
+        }
+        cout << nl;
+    }
+
+    return 0;
+}
