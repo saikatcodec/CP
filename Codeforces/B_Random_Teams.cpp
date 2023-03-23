90781b98b2b1f80b160fed3aa4e891f0500c34a1
Saikat Acharjee Joy <joy29940@gmail.com>
2023-03-23 23:59:39 +0600

    cf_random_team

---
+++ Codeforces/B_Random_Teams.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+
+int main() {
+    FIO;
+
+    ll n, m;
+    cin >> n >> m;
+    ll c = n - (m - 1) - 1;
+    ll vagfol = n / m;
+    ll vagsesteam = n % m;
+    ll vagfaolteam = m - (n % m);
+    cout << (((vagfol * (vagfol - 1)) / 2) * vagfaolteam) + ((vagfol * (vagfol + 1)) / 2) * vagsesteam << " " << (c * (c + 1)) / 2 << "\n";
+
+    return 0;
+}
