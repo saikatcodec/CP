94cc801418c7a29a21c79d265bf1c3888b37662c
Saikat Acharjee Joy <joy29940@gmail.com>
2023-03-24 23:26:16 +0600

    cses_counting_room

--- CSES/Counting_Rooms.cpp
+++ CSES/Counting_Rooms.cpp
@@ -18,39 +18,42 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 // clang-format on
 
-class Graph {
-    unordered_map<int, list<int>> g;
+int v;
+vector<vector<int>> g(1000005);
+vector<bool> visited(1000005, false);
+vector<int> cmp;
 
-    void dfs_helper(int st, unordered_map<int, bool> &visited) {
-        visited[st] = true;
+void dfs_helper(int st) {
+    visited[st] = true;
 
-        for (auto ele : g[st]) {
-            if (!visited[ele]) {
-                dfs_helper(ele, visited);
-            }
+    for (auto ele : g[st]) {
+        cmp.push_back(ele);
+        if (!visited[ele]) {
+            dfs_helper(ele);
+        }
+    }
+}
+
+void addEdges(int a, int b) {
+    g[a].push_back(b);
+    g[b].push_back(a);
+}
+
+int hasRoom() {
+    int cnt = 0;
+
+    for (int i = 0; i <= v; i++) {
+        if (!visited[i]) {
+            dfs_helper(i);
+        }
+        if (!cmp.empty()) {
+            cnt++;
+            cmp.clear();
         }
     }
 
-   public:
-    void addEdges(int a, int b) {
-        g[a].push_back(b);
-        g[b].push_back(a);
-    }
-
-    int hasRoom() {
-        unordered_map<int, bool> visited;
-        int cnt = 0;
-
-        for (auto node : g) {
-            if (!visited[node.first]) {
-                dfs_helper(node.first, visited);
-                cnt++;
-            }
-        }
-
-        return cnt;
-    }
-};
+    return cnt;
+}
 
 int main() {
     FIO;
@@ -63,23 +66,24 @@ int main() {
         cin >> grid[i];
     }
 
-    Graph g;
+    v = n * m;
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (grid[i][j] == '.') {
                 int cell = (i * m) + (j + 1);
-                g.addEdges(cell, cell);
+                addEdges(cell, cell);
                 if ((j + 1 < m) && (grid[i][j + 1] == '.')) {
-                    g.addEdges(cell, cell + 1);
+                    addEdges(cell, cell + 1);
                 }
                 if ((i + 1 < n) && (grid[i + 1][j] == '.')) {
-                    g.addEdges(cell, cell + m);
+                    addEdges(cell, cell + m);
                 }
             }
         }
     }
 
-    cout << g.hasRoom() << nl;
+    cout << hasRoom() << nl;
 
     return 0;
 }
