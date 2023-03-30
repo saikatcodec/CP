34fc6cc69531a21c9a31610fb77a411876995e46
Saikat Acharjee Joy <joy29940@gmail.com>
2023-03-30 23:46:49 +0600

    change

---
+++ Toph/Clock_Fantasy.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+// clang-format on
+
+int main() {
+    FIO;
+
+    testCase(t) {
+        double a, b;
+        cin >> a >> b;
+
+        double area = 0.5 * a * b;
+        cout << ps(area, 9) << nl;
+    }
+
+    return 0;
+}
