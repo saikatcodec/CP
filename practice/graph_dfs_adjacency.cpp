ae07e090960f6641e91251803c8e23d6e1d236fc
Saikat Acharjee Joy <joy29940@gmail.com>
2023-04-17 22:05:25 +0600

    practice_graph_dfs

--- practice/graph_dfs_adjacency.cpp
+++ practice/graph_dfs_adjacency.cpp
@@ -5,11 +5,19 @@ template <class T>
 class Graph {
     unordered_map<T, list<T>> g;
 
+    /**
+     * @param st starting point of graph
+     * @param visited the track of visited node
+     */
     void dfs_helper(T st, unordered_map<T, bool> &visited) {
+        // At first print the starting node
         cout << st << " ";
+        // Set starting point as true in visited map
         visited[st] = true;
 
+        // Traverse the adjacency list of the node such as 1: 3, 2, 5 
         for (auto node : g[st]) {
+            // if neighbour of the st is not visited then we call DFS recursively and continued
             if (!visited[node]) {
                 dfs_helper(node, visited);
             }
@@ -17,18 +25,23 @@ class Graph {
     }
 
    public:
+    // To add the edges
     void addEdges(T x, T y) {
         g[x].push_back(y);
         g[y].push_back(x);
     }
 
+    // Main part of DFS
     void dfs(T st) {
+        // To keep track of visited node 
         unordered_map<T, bool> visited;
 
+        // Traverse all the node one by one and set the nodes value false as default
         for (auto node_pair : g) {
             visited[node_pair.first] = false;
         }
 
+        // Call the DFS function to print the graph
         dfs_helper(st, visited);
         cout << endl;
     }
