55ed4efb733027612791c1a6fc7ce13296fe478b
Saikat Acharjee Joy <joy29940@gmail.com>
2023-04-28 22:42:06 +0600

    cf dijkstra

---
+++ Codeforces/C_Dijkstra.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+
+#define N 100001
+vector<int> parent(N, -1);
+vector<int> dist(N, INT_MAX);
+vector<vector<pair<int, int>>> adj(N);
+
+void addEdges(int a, int b, int c) {
+    adj[a].push_back({b, c});
+    adj[b].push_back({a, c});
+}
+
+void dijkstra(int src) {
+    set<pair<int, int>> p;
+
+    dist[src] = 0;
+    p.insert({dist[src], src});
+
+    while (!p.empty()) {
+        auto frnt = *(p.begin());
+        p.erase(p.begin());
+        int fntChild = frnt.second, fntDist = frnt.first;
+
+        for (auto ele : adj[fntChild]) {
+            if (fntDist + ele.second < dist[ele.first]) {
+                auto q = p.find({dist[ele.first], ele.first});
+                if (q != p.end()) {
+                    p.erase(q);
+                }
+
+                dist[ele.first] = fntDist + ele.second;
+                p.insert({dist[ele.first], ele.first});
+                parent[ele.first] = fntChild;
+            }
+        }
+    }
+}
+
+vector<int> getPath(int src, int dest) {
+    vector<int> path;
+
+    while (parent[dest] != -1) {
+        path.push_back(dest);
+        dest = parent[dest];
+    }
+
+    path.push_back(src);
+    reverse(path.begin(), path.end());
+
+    return path;
+}
+
+int main() {
+    FIO;
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        addEdges(a, b, c);
+    }
+
+    dijkstra(1);
+
+    if (dist[n] == INT_MAX) {
+        cout << -1 << nl;
+    } else {
+        vector<int> path = getPath(1, n);
+        for (auto ele : path) {
+            cout << ele << " ";
+        }
+        cout << nl;
+    }
+
+    return 0;
+}
