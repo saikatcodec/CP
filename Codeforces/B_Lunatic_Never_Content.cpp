fbe4c606dd8ae043dda3bf722327109e2f4b84c2
Saikat Acharjee Joy <joy29940@gmail.com>
2023-05-07 13:49:41 +0600

    cf lunatic never

---
+++ Codeforces/B_Lunatic_Never_Content.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+
+int main() {
+    FIO;
+
+    testCase(t) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = __gcd(ans, abs(arr[i] - arr[n - i - 1]));
+        }
+
+        cout << ans << nl;
+    }
+
+    return 0;
+}
