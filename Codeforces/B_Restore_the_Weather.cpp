7d791838565de0fdb4c4fb4669ccb67ff0f78fb2
Saikat Acharjee Joy <joy29940@gmail.com>
2023-05-23 15:27:10 +0600

    cf restor the weather

---
+++ Codeforces/B_Restore_the_Weather.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+
+int main() {
+    FIO;
+
+    testCase(t) {
+        int n, k;
+        cin >> n >> k;
+
+        vector<pair<int, int>> a(n);
+        for (int i = 0; i < n; i++) {
+            int tmp;
+            cin >> tmp;
+            a[i].first = tmp;
+            a[i].second = i;
+        }
+
+        int b[n];
+        for (int i = 0; i < n; i++) {
+            cin >> b[i];
+        }
+
+        sort(a.begin(), a.end());
+        sort(b, b + n);
+
+        int ans[n];
+        for (int i = 0; i < n; i++) {
+            ans[a[i].second] = b[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            cout << ans[i] << " ";
+        }
+        cout << nl;
+    }
+
+    return 0;
+}
