e9c90355f40e2c9cb2136b53e91918892d231b24
Saikat Acharjee Joy <joy29940@gmail.com>
2023-06-21 01:23:06 +0600

    cf apple

--- Codeforces/C_Sum_in_Binary_Tree.cpp
+++ Codeforces/C_Sum_in_Binary_Tree.cpp
@@ -18,53 +18,10 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 // clang-format on
 
-#define N 200002
-map<ll, list<ll>> tr;
-ll leaf[N];
-
-ll dfs(ll src, ll prnt) {
-    auto p = tr[src];
-    if (p.size() == 1 && (*p.begin()) == prnt) {
-        return leaf[src] = 1;
-    }
-
-    ll val = 0;
-    for (auto e : p) {
-        if (e != prnt) {
-            val += dfs(e, src);
-        }
-    }
-
-    return leaf[src] = val;
-}
-
 int main() {
     FIO;
 
     testCase(t) {
-        tr.clear();
-        ll n;
-        cin >> n;
-
-        for (ll i = 0; i < n - 1; i++) {
-            ll a, b;
-            cin >> a >> b;
-
-            tr[a].push_back(b);
-            tr[b].push_back(a);
-        }
-
-        dfs(1, 0);
-
-        ll q;
-        cin >> q;
-
-        while (q--) {
-            ll x, y;
-            cin >> x >> y;
-
-            cout << leaf[x] * leaf[y] << nl;
-        }
     }
 
     return 0;
