1774f6a1813b49f06b726e78e206f507c4f12898
Joy Kumar Acharjee <joy29940@gmail.com>
2023-06-26 22:15:16 +0600

    trie unique prefix

--- practice/Trie_find_unique_prefix.cpp
+++ practice/Trie_find_unique_prefix.cpp
@@ -1,45 +1,103 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-   public:
+class Node
+{
+public:
     char data;
     unordered_map<char, Node *> children;
     bool leaf;
     int cnt = 0;
 
-    Node(char ch) {
+    Node(char ch)
+    {
         data = ch;
         leaf = false;
         cnt = 1;
     }
 };
 
-class Trie {
+class Trie
+{
     Node *root;
 
-   public:
-    Trie() {
+public:
+    Trie()
+    {
         root = new Node('\0');
     }
 
-    void insert(string str) {
+    void insert(string str)
+    {
         Node *temp = root;
-        for (int i = 0; i < str.size(); i++) {
+        for (int i = 0; i < str.size(); i++)
+        {
             char ch = str[i];
 
-            if (temp->children.count(ch)) {
+            if (temp->children.count(ch))
+            {
                 temp->children[ch]->cnt += 1;
                 temp = temp->children[ch];
-            } else {
+            }
+            else
+            {
                 Node *newIn = new Node(ch);
                 temp->children[ch] = newIn;
                 temp = newIn;
             }
         }
+
+        temp->leaf = true;
+    }
+
+    string uniquePrefix(string str)
+    {
+        Node *temp = root;
+        string ans = "";
+        for (int i = 0; i < str.size(); i++)
+        {
+            char ch = str[i];
+            ans.push_back(ch);
+
+            if (temp->children[ch]->cnt == 1)
+            {
+                return ans;
+            }
+            else
+            {
+                temp = temp->children[ch];
+            }
+        }
+
+        if (temp->cnt != 1)
+        {
+            ans = "-1";
+        }
+        return ans;
     }
 };
 
-int main() {
+int main()
+{
+    string arr[] = {"cobra", "duck", "dove", "dog", "cobras"};
+
+    Trie tr;
+    for (int i = 0; i < 5; i++)
+    {
+        tr.insert(arr[i]);
+    }
+
+    vector<string> ans;
+    for (int i = 0; i < 5; i++)
+    {
+        string tmp = tr.uniquePrefix(arr[i]);
+        ans.push_back(tmp);
+    }
+
+    for (auto el : ans)
+    {
+        cout << el << endl;
+    }
+
     return 0;
 }
