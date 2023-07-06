83a1c561eabc25d7890a8b4e7c9e27c23b292375
Joy Kumar Acharjee <joy29940@gmail.com>
2023-07-07 02:45:04 +0600

    boredom

---
+++ Codeforces/A_Boredom.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+#define N 100005
+map<ll, ll> cnt, dp;
+
+int main() {
+    FIO;
+
+    ll n;
+    cin >> n;
+
+    ll arr[n];
+    for (ll i = 0; i < n; i++) {
+        cin >> arr[i];
+        cnt[arr[i]]++;
+    }
+
+    dp[0] = 0;
+    dp[1] = cnt[1];
+
+    for (ll i = 2; i <= N; i++) {
+        dp[i] = max(dp[i - 1], dp[i - 2] + cnt[i] * i);
+    }
+    cout << dp[N] << nl;
+
+    return 0;
+}
