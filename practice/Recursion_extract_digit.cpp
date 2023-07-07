6b9c7b0c1ac0c31676372257c359e7cde3237c5e
Joy Kumar Acharjee <joy29940@gmail.com>
2023-07-07 10:44:05 +0600

    modified

--- practice/Recursion_extract_digit.cpp
+++ practice/Recursion_extract_digit.cpp
@@ -1,33 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string word[10] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+
 void extractDigit(int n) {
     if (n == 0) {
         return;
     }
     extractDigit(n / 10);
-
-    if (n % 10 == 1) {
-        cout << "One ";
-    } else if (n % 10 == 2) {
-        cout << "Two ";
-    } else if (n % 10 == 3) {
-        cout << "Three ";
-    } else if (n % 10 == 4) {
-        cout << "Four ";
-    } else if (n % 10 == 5) {
-        cout << "Five ";
-    } else if (n % 10 == 6) {
-        cout << "Six ";
-    } else if (n % 10 == 7) {
-        cout << "Seven ";
-    } else if (n % 10 == 8) {
-        cout << "Eight ";
-    } else if (n % 10 == 9) {
-        cout << "Nine ";
-    } else if (n % 10 == 0) {
-        cout << "Zero ";
-    }
+    cout << word[n % 10] << " ";
 }
 
 int main() {
