5f421c15789cffe337948cc8db277995c1113659
Joy Kumar Acharjee <joy29940@gmail.com>
2023-07-08 13:29:02 +0600

    change

--- Algo_lab/pp.cpp
+++ Algo_lab/pp.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int rodCut(int price[], int len, vector<int> &dp) {
-    if (dp[len] == INT_MIN) {
+int rodCut(int price[], int len, int dp[]) {
+    if (dp[len] == -1) {
         int q = INT_MIN; 
         for (int i = 1; i <= len; i++) {
             q = max(q, price[i - 1] + rodCut(price, len - i, dp));
         }
-
         dp[len] = q;
     }
-
     return dp[len];
 }
 
@@ -23,7 +21,10 @@ int main() {
         cin >> price[i];
     }
 
-    vector<int> dp(len + 1, INT_MIN);
+    int dp[len+1];
+    for(int i =0; i<=len; i++){
+        dp[i]=-1;
+    }
     dp[0] = 0;
 
     cout << "Answer: " << rodCut(price, len, dp) << endl;
