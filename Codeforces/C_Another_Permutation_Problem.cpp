96523e2167903ab5b9e8eb64e5cee094ef50bf8f
Joy Kumar Acharjee <joy29940@gmail.com>
2023-08-13 12:52:49 +0600

    cf

--- Codeforces/C_Another_Permutation_Problem.cpp
+++ Codeforces/C_Another_Permutation_Problem.cpp
@@ -18,18 +18,38 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 // clang-format on
 
-ll check(ll n) {
-    if (n == 0) {
-        return 0;
-    }
-}
-
 int main() {
     FIO;
 
     testCase(t) {
         ll n;
         cin >> n;
+
+        if (n == 2) {
+            cout << 2 << nl;
+            continue;
+        }
+
+        ll ans = 0;
+        for (ll i = 1; i <= n; i++) {
+            ll sum = 0, maxx = 0;
+            for (ll j = 1; j <= i; j++) {
+                sum += (j * j);
+                maxx = max(maxx, j * j);
+            }
+
+            ll ele = n;
+            for (ll j = i + 1; j <= n; j++) {
+                sum += (j * ele);
+                maxx = max(maxx, j * ele);
+                ele--;
+            }
+
+            sum -= maxx;
+            ans = max(ans, sum);
+        }
+
+        cout << ans << nl;
     }
 
     return 0;
