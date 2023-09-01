f56552df0976081ec0361ccf4554a92a81f9ecc8
Joy Kumar Acharjee <joy29940@gmail.com>
2023-09-01 13:38:16 +0600

    modified

--- Camp/D_Update_the_array.cpp
+++ Camp/D_Update_the_array.cpp
@@ -18,48 +18,73 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 // clang-format on
 
-vector<int> tr;
+vector<int> tr, lz;
 void update(int s, int e, int l, int r, int val, int ind) {
-    if (r < s || l > e) {
+    if (lz[ind] != 0) {
+        tr[ind] += lz[ind];
+        if (s != e) {
+            lz[2 * ind] += lz[ind];
+            lz[2 * ind + 1] += lz[ind];
+        }
+        lz[ind] = 0;
+    }
+
+    // base case
+    if (l > e || r < s) {
         return;
     }
 
-    if (s == e) {
+    if (l <= s && e <= r) {
         tr[ind] += val;
+        if (s != e) {
+            lz[2 * ind] += val;
+            lz[2 * ind + 1] += val;
+        }
         return;
     }
 
     int m = (s + e) >> 1;
     update(s, m, l, r, val, 2 * ind);
     update(m + 1, e, l, r, val, 2 * ind + 1);
-    tr[ind] = min(tr[2 * ind], tr[2 * ind + 1]);
 }
 
-int query(int s, int e, int l, int r, int ind) {
-    if (l <= s && e <= r) {
-        return tr[ind];
+void query(int s, int e, int l, int ind) {
+    if (lz[ind] != 0) {
+        tr[ind] += lz[ind];
+        if (s != e) {
+            lz[2 * ind] += lz[ind];
+            lz[2 * ind + 1] += lz[ind];
+        }
+        lz[ind] = 0;
     }
 
-    if (l > e || r < s) {
-        return INT_MAX;
+    if (l > e || l < s) {
+        return;
+    }
+
+    if (s == e) {
+        cout << tr[ind] << nl;
+        return;
     }
 
     int m = (s + e) >> 1;
-    int lf = query(s, m, l, r, 2 * ind);
-    int rt = query(m + 1, e, l, r, 2 * ind + 1);
-    return min(lf, rt);
+    query(s, m, l, 2 * ind);
+    query(m + 1, e, l, 2 * ind + 1);
 }
 
 int main() {
     FIO;
 
     testCase(t) {
+        tr.clear();
+        lz.clear();
         int n, u;
         cin >> n >> u;
 
-        tr.clear();
         tr.resize(4 * n + 1, 0);
-        for (int i = 0; i < u; i++) {
+        lz.resize(4 * n + 1, 0);
+
+        while (u--) {
             int l, r, val;
             cin >> l >> r >> val;
 
@@ -72,8 +97,7 @@ int main() {
         while (q--) {
             int i;
             cin >> i;
-
-            cout << query(0, n - 1, i, i, 1) << nl;
+            query(0, n - 1, i, 1);
         }
     }
 
