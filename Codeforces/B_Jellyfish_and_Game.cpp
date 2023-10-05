8f6d8ab2d38908f1f733d61ccf59cf9aca83b6a3
Joy Kumar Acharjee <joy29940@gmail.com>
2023-10-05 22:25:43 +0600

    cf

---
+++ Codeforces/B_Jellyfish_and_Game.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define int long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+
+int32_t main() {
+    FIO;
+
+    testCase(t) {
+        int n, m, k;
+        cin >> n >> m >> k;
+
+        int a[n], b[m];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (int i = 0; i < m; i++) {
+            cin >> b[i];
+        }
+
+        int mx1 = *max_element(a, a + n);
+        int mx2 = *max_element(b, b + m);
+        int mn1 = *min_element(a, a + n);
+        int mn2 = *min_element(b, b + m);
+
+        int sum = 0;
+        sum = accumulate(a, a + n, sum);
+
+        sum += max(mx2 - mn1, 0LL);
+        if (k % 2 == 0) {
+            sum -= (max(mx1, mx2) - min(mn1, mn2));
+        }
+        cout << sum << nl;
+    }
+
+    return 0;
+}
