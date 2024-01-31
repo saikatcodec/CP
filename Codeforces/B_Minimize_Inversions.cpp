c377957369cdd6e8745f839899eac9e7352a0c65
Joy Kumar Acharjee <joy29940@gmail.com>
2024-01-31 15:12:46 +0600

    cf

---
+++ Codeforces/B_Minimize_Inversions.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+
+int main() {
+    FIO;
+
+    testCase(t) {
+        int n;
+        cin >> n;
+
+        set<pair<int, int>> st;
+        int a[n], b[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> b[i];
+            st.insert({a[i], b[i]});
+        }
+
+        for (auto p : st) {
+            cout << p.first << " ";
+        }
+        cout << nl;
+        for (auto p : st) {
+            cout << p.second << " ";
+        }
+        cout << nl;
+    }
+
+    return 0;
+}
