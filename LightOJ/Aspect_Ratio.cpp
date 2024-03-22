b2ec532a8aaf12ea70105f6466a6c8aa182eb820
Joy Kumar Acharjee <joy29940@gmail.com>
2024-03-22 22:24:21 +0600

    light oj

---
+++ LightOJ/Aspect_Ratio.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+
+int main() {
+    FIO;
+
+    int c = 0;
+    testCase(t) {
+        double k;
+        cin >> k;
+
+        double r = sqrt(((k * k) - 1) / (4 - (k * k)));
+        cout << "Case " << ++c << ": " << ps(r, 4) << nl;
+    }
+
+    return 0;
+}
