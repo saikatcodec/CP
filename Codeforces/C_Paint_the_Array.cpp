d8a850bb062e68df9eb69070d43a72d5c2b53503
Joy Kumar Acharjee <joy29940@gmail.com>
2024-03-24 22:50:16 +0600

    cf unsolved

---
+++ Codeforces/C_Paint_the_Array.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+
+int main() {
+    FIO;
+
+    testCase(t) {
+        int n;
+        cin >> n;
+
+        ll arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        ll gm = 0;
+        for (int i = 0; i < n; i += 2) {
+            gm = __gcd(gm, arr[i]);
+        }
+
+        ll gg = 0;
+        for (int i = 1; i < n; i += 2) {
+            gg = __gcd(gg, arr[i]);
+        }
+
+        cerr << gg << " " << gm << nl;
+
+        bool flag1 = true, flag2 = true;
+        for (int i = 0; i < n; i += 2) {
+            if (arr[i] % gg == 0) {
+                flag1 = false;
+                break;
+            }
+        }
+
+        for (int i = 1; i < n; i += 2) {
+            if (arr[i] % gm == 0) {
+                flag2 = false;
+                break;
+            }
+        }
+
+        if (gm == gg) {
+            cout << 0 << nl;
+        } else {
+                }
+        if (flag1 && flag2) {
+            cout << max(gm, gg) << nl;
+        } else {
+            cout << 0 << nl;
+        }
+    }
+
+    return 0;
+}
