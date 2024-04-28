2b389acf875817fef3af9352e5a76092636d031a
Joy Kumar Acharjee <joy29940@gmail.com>
2024-04-28 21:51:02 +0600

    cf

---
+++ Codeforces/C_Everything_Nim.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+// clang-format off
+#define nl "\n"
+#define ll long long
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define testCase(x) int x; cin >> x; while (x--)
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// clang-format on
+
+int main() {
+    FIO;
+
+    testCase(T) {
+        int n;
+        cin >> n;
+
+        set<int> st;
+        for (int i = 0; i < n; i++) {
+            int a;
+            cin >> a;
+            st.insert(a);
+        }
+
+        vector<int> v;
+        for (auto ele : st) {
+            v.push_back(ele);
+        }
+
+        vector<int> res;
+        res.push_back(v[0]);
+        for (int i = 1; i < v.size(); i++) {
+            res.push_back(v[i] - v[i - 1]);
+        }
+
+        int ans = 1;
+        for (int i = res.size() - 2; i >= 0; i--) {
+            if (res[i] == 1) {
+                ans = 1 - ans;
+            } else {
+                ans = 1;
+            }
+        }
+
+        if (ans == 1) {
+            cout << "Alice" << nl;
+        } else {
+            cout << "Bob" << nl;
+        }
+    }
+
+    return 0;
+}
